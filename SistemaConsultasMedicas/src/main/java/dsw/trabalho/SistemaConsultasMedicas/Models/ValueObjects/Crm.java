76c67bd7c5dd890613a477b04305a1c41d7f04c2
MarsCristian <cristian.martins.ccm@gmail.com>
2023-08-26 04:18:10 -0300

    implementar valueobject crm

--- SistemaConsultasMedicas/src/main/java/dsw/trabalho/SistemaConsultasMedicas/Models/ValueObjects/Crm.java
+++ SistemaConsultasMedicas/src/main/java/dsw/trabalho/SistemaConsultasMedicas/Models/ValueObjects/Crm.java
@@ -1,4 +1,28 @@
 package dsw.trabalho.SistemaConsultasMedicas.Models.ValueObjects;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Value;
+
+import java.util.regex.Pattern;
+
+@Value
+@Schema(implementation = String.class, description = "CRM")
 public class Crm {
+    String crmNumber;
+
+    public Crm(String crm) {
+        this.crmNumber = validateCRM(crm);
+
+    }
+
+    private static String validateCRM(String crm) {
+        String regex = "\\d{6}CRM/[A-Z]{2}";
+        Pattern pattern = Pattern.compile(regex);
+
+        if(!pattern.matcher(crm).matches()){
+            throw new IllegalArgumentException("Formato de CRM inválido");
+        }
+
+        return crm;
+    }
 }
