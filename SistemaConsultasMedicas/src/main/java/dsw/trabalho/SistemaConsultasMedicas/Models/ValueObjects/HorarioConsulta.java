6603780955160807cc7adeec9ebdcac8fb2619c8
Lucas Balieiro <lucas.balieiro@estudante.ufscar.br>
2023-08-28 13:10:35 -0300

    Create HorarioConsulta.java

---
+++ SistemaConsultasMedicas/src/main/java/dsw/trabalho/SistemaConsultasMedicas/Models/ValueObjects/HorarioConsulta.java
@@ -0,0 +1,20 @@
+package dsw.trabalho.SistemaConsultasMedicas.Models.ValueObjects;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Value;
+
+@Value
+@Schema(implementation = String.class, description = "horarioConsulta")
+public class HorarioConsulta {
+
+    String horarioNumber;
+
+    public HorarioConsulta(String horario){
+        this.horarioNumber = validateHorario(horario);
+    }
+
+    public static String validateHorario(String horario){
+        //todo montar o validate hora
+    }
+}
