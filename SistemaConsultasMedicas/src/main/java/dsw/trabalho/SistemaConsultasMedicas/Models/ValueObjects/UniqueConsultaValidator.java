b9e3a184d11ee8111f854067871c57d3be90443c
Lucas Balieiro <lucas.balieiro@estudante.ufscar.br>
2023-08-28 12:50:59 -0300

    Update UniqueConsultaValidator.java

--- SistemaConsultasMedicas/src/main/java/dsw/trabalho/SistemaConsultasMedicas/Models/ValueObjects/UniqueConsultaValidator.java
+++ SistemaConsultasMedicas/src/main/java/dsw/trabalho/SistemaConsultasMedicas/Models/ValueObjects/UniqueConsultaValidator.java
@@ -1,4 +1,4 @@
-package dsw.trabalho.SistemaConsultasMedicas.Models.Validators;
+package dsw.trabalho.SistemaConsultasMedicas.Models.ValueObjects;
 
 import dsw.trabalho.SistemaConsultasMedicas.Models.Entities.ConsultaModel;
 import dsw.trabalho.SistemaConsultasMedicas.Models.ValueObjects.Cpf;
@@ -6,6 +6,7 @@ import dsw.trabalho.SistemaConsultasMedicas.Persistence.IConsultaDao;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import dsw.trabalho.SistemaConsultasMedicas.Models.Validators.UniqueConsulta;
 
 public class UniqueConsultaValidator implements ConstraintValidator<UniqueConsulta, ConsultaModel> {
 
@@ -19,8 +20,16 @@ public class UniqueConsultaValidator implements ConstraintValidator<UniqueConsul
 
         if(dao != null){
             ConsultaModel consulta = dao.findByDataConsultaAndHorarioConsulta(data, hora);
-            return consulta == null;
-        }else{
+
+            if(consulta != null){
+                if(value.getMedico() == consulta.getMedico() || value.getPaciente() == consulta.getPaciente()){
+                    return false;
+                }
+            }else{
+                return true;
+            }
+        }
+        else{
             return true;
         }
     }
